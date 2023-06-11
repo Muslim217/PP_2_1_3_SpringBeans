a6f77c2f208076af587ef84848cbeeb6883dc95b
Muslim217 <abumuslimsafaraliev3815@gmail.com>
2023-06-11 15:20:45 +0300

    Version1

--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -1,8 +1,11 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class Cat extends Animal {
     @Override
     public String toString() {
